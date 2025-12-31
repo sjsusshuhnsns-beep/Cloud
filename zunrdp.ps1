75e3d4291c053b64b6c925397369645b23b57fa6
vpsf0605-dev <vpsf0605@gmail.com>
2026-01-01 06:57:56 +0700

    Update zunrdp.ps1

--- zunrdp.ps1
+++ zunrdp.ps1
@@ -1,23 +1,29 @@
 Param ([string]$Owner, [string]$MachineID)
 $baseUrl = "https://zunrdp-default-rtdb.asia-southeast1.firebasedatabase.app"
-$vmUrl = "$baseUrl/vms/$MachineID.json"
-$cmdUrl = "$baseUrl/commands/$MachineID.json"
 $pass = (Get-Content "pass.txt" -Raw).Trim()
+$startTime = (Get-Content "uptime.txt" -Raw).Trim()
 
 while($true) {
     try {
-        $cmd = Invoke-RestMethod -Uri $cmdUrl -Method Get -ErrorAction SilentlyContinue
+        # Check Ban/Stop
+        $cmd = Invoke-RestMethod -Uri "$baseUrl/commands/$MachineID.json" -Method Get -ErrorAction SilentlyContinue
         if ($cmd.action -eq "stop") {
-            Invoke-RestMethod -Uri $cmdUrl -Method Delete
-            Invoke-RestMethod -Uri $vmUrl -Method Delete
+            Invoke-RestMethod -Uri "$baseUrl/commands/$MachineID.json" -Method Delete
+            Invoke-RestMethod -Uri "$baseUrl/vms/$MachineID.json" -Method Delete
             Stop-Computer -Force; exit
         }
+
         $cpu = [Math]::Round((Get-WmiObject Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average, 1)
         $ram = [Math]::Round(((Get-WmiObject Win32_OperatingSystem | % { ($_.TotalVisibleMemorySize - $_.FreePhysicalMemory) / $_.TotalVisibleMemorySize }) * 100), 1)
         $ip = (& "C:\Program Files\Tailscale\tailscale.exe" ip -4).Trim()
 
-        $data = @{ id=$MachineID; ip=$ip; owner=$Owner; user="ZunRDP"; pass=$pass; cpu=$cpu; ram=$ram; lastSeen=[DateTimeOffset]::Now.ToUnixTimeMilliseconds() } | ConvertTo-Json
-        Invoke-RestMethod -Uri $vmUrl -Method Put -Body $data
+        $data = @{ 
+            id=$MachineID; ip=$ip; owner=$Owner; user="ZunRDP"; pass=$pass; 
+            cpu=$cpu; ram=$ram; startTime=$startTime; 
+            lastSeen=[DateTimeOffset]::Now.ToUnixTimeMilliseconds() 
+        } | ConvertTo-Json
+        
+        Invoke-RestMethod -Uri "$baseUrl/vms/$MachineID.json" -Method Put -Body $data
     } catch { }
     Start-Sleep -Seconds 5
 }
